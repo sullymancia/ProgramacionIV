a6e1721b2636f9e99ac140ce74f5e3b9b47ccdda
Brandon Napky <coco.napky@gmail.com>
2015-08-23 17:17:48 -0600

    added index view

--- GestionVacacionesUnitec/GestionVacacionesUnitec/App_Start/RouteConfig.cs
+++ GestionVacacionesUnitec/GestionVacacionesUnitec/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GestionVacacionesUnitec
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "TestForService", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
